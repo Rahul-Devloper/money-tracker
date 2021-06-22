e5a2773e47a9a7a9dbd9da1b10da67508e56f28c
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-06-23 00:57:41 +0530

    added expenses page

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import '../components/ExpenseDate.css'
-const ExpenseDate = (props) => {
+import './ExpenseDate.css';
 
+const ExpenseDate = (props) => {
 const month = props.date.toLocaleString('en-US',{month: 'long'});
 const year = props.date.toLocaleString('en-US',{day: '2-digit'});
 const date = props.date.getFullYear();
@@ -9,10 +9,10 @@ const date = props.date.getFullYear();
 
 
     return (
-        <div className = "expense_date">
-            <div className = "expense_date_month">{month}</div>
-            <div className = "expense_date_year">{year}</div>
-            <div className = "expense_date_day">{date}</div>
+        <div className = "expense-date">
+            <div className = "expense-date_month">{month}</div>
+            <div className = "expense-date_year">{year}</div>
+            <div className = "expense-date_day">{date}</div>
         </div>
     )
 }
