8f29400b4c49bdbb1cd327bfdfe45f6c12e45868
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-06-29 01:35:06 +0530

    multiple state management

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -30,7 +31,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items = {expenses}/>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 import Card from './Card'
@@ -5,15 +6,23 @@ import Card from './Card'
 
 function ExpenseItem(props) {
 
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = ()=>{
+    setTitle('updated');
+  }
+
   return (
     <Card className='expense-item'>
       
         <ExpenseDate date = {props.date}/>
      
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Update</button>
     </Card>
   );
 }
